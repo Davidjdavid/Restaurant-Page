c9588aa2a1a14ef5b074e82cd0981f0968a1b19d
David <dave.j.hazelwood@gmail.com>
2024-09-15 15:17:18 -0500

    Everything working, uploading to GitHub Pages to configure that before styling

--- about.js
+++ about.js
@@ -1,10 +1,9 @@
 import "./styles.css";
-import bread from "./bread.jpg";
 
-let homeDiv = document.createElement("div");
-let homeHeader = document.createElement("h2");
-homeHeader.id = "pageTitle";
-homeHeader.innerText = "Home";
+let aboutDiv = document.createElement("div");
+let aboutHeader = document.createElement("h2");
+aboutHeader.id = "pageTitle";
+aboutHeader.innerText = "About";
 
 let pageContentOne = document.createElement("p");
 pageContentOne.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices magna at pharetra lobortis. In aliquet est fermentum, vehicula tellus eu, elementum leo. sit amet. Phasellus tincidunt tincidunt erat nec viverra. Praesent eu metus sed leo luctus accumsan in sit amet eros. Nulla euismod eleifend vestibulum. Vivamus tincidunt velit non ante aliquet ultricies. In nec tempus arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices magna at pharetra lobortis. In aliquet est fermentum, vehicula tellus eu, elementum leo.  Morbi ut justo aliquet, tempor risus nec, fermentum diam. Nulla semper posuere erat, eu semper dolor egestas ac. Duis pretium mattis purus, id congue arcu lobortis  sit amet. Phasellus tincidunt tincidunt erat nec viverra. Praesent eu metus sed leo luctus accumsan in sit amet eros. Nulla euismod eleifend vestibulum. Praesent commodo accumsan odio, ac mollis velit sagittis id. Phasellus cursus tincidunt libero at fringilla. Proin in gravida metus. Vivamus in ultrices magna. Integer  vitae molestie erat. Vestibulum nulla urna, pellentesque nec est et, tincidunt consectetur ligula. In placerat felis a mauris venenatis, a dapibus sapien finibus. Suspendisse fringilla tempus magna eu pulvinar. Vestibulum pharetra diam eu nunc porta molestie. Nunc quam metus, dictum ut vehicula nec, vestibulum a justo. Cras in tincidunt eros. Phasellus placerat mauris elit, eget vulputate risus imperdiet ut. Ut dapibus turpis interdum, lobortis diam sed, gravida lorem. Integer dignissim pretium elementum. Morbi fermentum aliquet augue eget placerat. Morbi ut justo aliquet, tempor risus nec, fermentum diam. Nulla semper posuere erat, eu semper dolor egestas ac. Duis pretium mattis purus, id congue arcu lobortis"
@@ -12,19 +11,15 @@ pageContentOne.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing e
 let pageContentTwo = document.createElement("p");
 pageContentTwo.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices magna at pharetra lobortis. In aliquet est fermentum, vehicula tellus eu, elementum leo. sit amet. Phasellus tincidunt tincidunt erat nec viverra. Praesent eu metus sed leo luctus accumsan in sit amet eros. Nulla euismod eleifend vestibulum. Vivamus tincidunt velit non ante aliquet ultricies. In nec tempus arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices magna at pharetra lobortis. In aliquet est fermentum, vehicula tellus eu, elementum leo.  Morbi ut justo aliquet, tempor risus nec, fermentum diam. Nulla semper posuere erat, eu semper dolor egestas ac. Duis pretium mattis purus, id congue arcu lobortis  sit amet. Phasellus tincidunt tincidunt erat nec viverra. Praesent eu metus sed leo luctus accumsan in sit amet eros. Nulla euismod eleifend vestibulum. Praesent commodo accumsan odio, ac mollis velit sagittis id. Phasellus cursus tincidunt libero at fringilla. Proin in gravida metus. Vivamus in ultrices magna. Integer  vitae molestie erat. Vestibulum nulla urna, pellentesque nec est et, tincidunt consectetur ligula. In placerat felis a mauris venenatis, a dapibus sapien finibus. Suspendisse fringilla tempus magna eu pulvinar. Vestibulum pharetra diam eu nunc porta molestie. Nunc quam metus, dictum ut vehicula nec, vestibulum a justo. Cras in tincidunt eros. Phasellus placerat mauris elit, eget vulputate risus imperdiet ut. Ut dapibus turpis interdum, lobortis diam sed, gravida lorem. Integer dignissim pretium elementum. Morbi fermentum aliquet augue eget placerat. Morbi ut justo aliquet, tempor risus nec, fermentum diam. Nulla semper posuere erat, eu semper dolor egestas ac. Duis pretium mattis purus, id congue arcu lobortis"
 
-let breadImage = document.createElement("img");
-breadImage.src = bread;
-
 let firstRow = document.createElement("div");
 let secondRow = document.createElement("div");
 
 
-homeDiv.appendChild(homeHeader);
-homeDiv.appendChild(firstRow);
-homeDiv.appendChild(secondRow);
+aboutDiv.appendChild(aboutHeader);
+aboutDiv.appendChild(firstRow);
+aboutDiv.appendChild(secondRow);
 
-firstRow.appendChild(breadImage);
 firstRow.appendChild(pageContentOne);
 secondRow.appendChild(pageContentTwo);
 
-export { homeDiv };
+export { aboutDiv };
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 import "./styles.css";
-import { mainDiv } from "./home.js";
+import { homeDiv } from "./home.js";
+import { menuDiv } from "./menu.js";
+import { contactDiv } from "./contact.js";
+import { aboutDiv } from "./about.js";
 
-let homeBtn = document.querySelector(".home");
+
 let displayContainer = document.getElementById("content");
+let homeBtn = document.querySelector(".home");
+let menuBtn = document.querySelector(".menu");
+let aboutBtn = document.querySelector(".about");
+let contactBtn = document.querySelector(".contact");
+
 
 homeBtn.addEventListener("click", ()=> {
     displayContainer.textContent = "";
-    displayContainer.appendChild(mainDiv);
+    displayContainer.appendChild(homeDiv);
+})
+
+menuBtn.addEventListener("click", ()=> {
+    displayContainer.textContent = "";
+    displayContainer.appendChild(menuDiv);
+})
+
+aboutBtn.addEventListener("click", ()=> {
+    displayContainer.textContent = "";
+    displayContainer.appendChild(contactDiv);
+})
+
+contactBtn.addEventListener("click", ()=> {
+    displayContainer.textContent = "";
+    displayContainer.appendChild(aboutDiv);
 })
